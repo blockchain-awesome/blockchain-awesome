c0058ad2039f706ed4a0c2721f06c34650a6e0dc
calidion <calidion@gmail.com>
2018-01-23 23:44:06 +0800

    updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -11,26 +11,6 @@ export class HomePage {
   item: object;
   constructor(public navCtrl: NavController,
     private http: HttpClient) {
-    // this.items = [{
-    //   name: 'Hello World',
-    //   url: '',
-    //   icon: ''
-    // }, {
-    //   name: 'Hello World',
-    //   url: '',
-    //   icon: '',
-    //   items: [
-    //     {
-    //       name: 'Hello World 1',
-    //       url: '',
-    //       icon: ''
-    //     }, {
-    //       name: 'Hello World 2',
-    //       url: '',
-    //       icon: ''
-    //     }
-    //   ]
-    // }];
   }
 
   ngOnInit() {
